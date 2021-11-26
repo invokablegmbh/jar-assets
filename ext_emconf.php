deb0a0a71b43045b7d768a685c164b0f59e362fa
Julian Lichte <julian.lichte@jar.media>
2021-11-26 14:31:15 +0100

    Initial Commit

---
+++ ext_emconf.php
@@ -0,0 +1,25 @@
+<?php
+
+$EM_CONF['jar_assets'] = array(
+	'title' => 'Multiple Assets',
+	'description' => 'Includes all CSS and JavaScript files of a folder.',
+	'category' => 'plugin',
+	'author' => 'JAR Media GmbH',
+	'author_email' => 'info@jar.media',
+	'version' => '1.0.0',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => '0',
+	'createDirs' => '',
+	'clearCacheOnLoad' => 0,
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '10.4',
+			'php' => '7.4.0-7.4.999',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+);
