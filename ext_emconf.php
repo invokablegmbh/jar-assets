5774011b756187acdacbc3aa1c713ebe49c2bade
Julian Lichte <julian.lichte@jar.media>
2023-10-13 08:04:55 +0000

    Fixed publishing to TER

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,7 +6,7 @@ $EM_CONF['jar_assets'] = array(
 	'category' => 'plugin',
 	'author' => 'invokable GmbH',
 	'author_email' => 'info@invokable.gmbh',
-	'version' => '1.0.7',
+	'version' => '1.0.8',
 	'state' => 'stable',
 	'internal' => '',
 	'uploadfolder' => '0',
@@ -15,7 +15,6 @@ $EM_CONF['jar_assets'] = array(
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '10.4.1-11.5.99',
-			'php' => '7.4.0-7.4.999',
 			'jar_utilities' => '1.0.0'
 		),
 		'conflicts' => array(
